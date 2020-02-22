6d53792a1209621472bf3ed5f86827ae936850c2
Caryl Henry <carolynhenry2021@u.northwestern.edu>
2020-02-22 16:05:04 -0600

    Added psuedocode in comments

--- distance_vector_node.py
+++ distance_vector_node.py
@@ -9,40 +9,50 @@ class Distance_Vector_Node(Node):
         self.dv = {}
         self.seq_num = {}
 
-   
     # Need a function to update link costs which will also update DV
 
     # Need a function to update personal DV
 
     # Need a function to check for shorter times from other DVS after cost or personal DV is changed
 
-
     # Return a string
+
     def __str__(self):
         return "Rewrite this function to define your node dump printout"
 
-    # Fill in this function
-    # Called to inform you that an outgoing link connected to your node has just changed its properties.  
-    # It tells you that you can reach a certain neighbor (identified by an integer) with a certain latency.  
-    # In response, you may want to update your tables and send further messages to your neighbors.  
-
-    # Going to want to update link costs here and send the new DV to all neighbors
+    # Called to inform you that an outgoing link to a neighbor has changed.
+    # It tells you that you can reach a certain neighbor (identified by an integer) with a certain latency.
+    # In response, you may want to update your tables and send further messages to your neighbors.
     def link_has_been_updated(self, neighbor, latency):
-        # latency = -1 if delete a link
+        # if latency == -1 delete a link
+            # remove entries from the DV where the next hop is this link
+        # else: link has gotten longer or shorter
+            # find the difference between the original and new latency
+            # add this difference to the cost of all paths where this neighbor is the first hop
+        # updates one's own sequence number
+        # send out DV with new sequence number
         pass
 
-    # Fill in this function
-    # Called when a routing message "m" arrives at a node.  
-    # This message would have been sent by a neighbor (more about how to do that later).  
-    # The message is a string.  
-    # In response, you may send further routing messages using self.send_to_neighbors or self.send_to_neighbor. 
+    # Called when a routing message "m" arrives at a node.
+    # This STRING message would have been sent by a neighbor
+    # In response, you may send further routing messages using self.send_to_neighbors or self.send_to_neighbor.
     # You may also update your tables.
-    
-    # Going to want to update DV here
     def process_incoming_routing_message(self, m):
+        # get neighbor number and sequenuce number from message
+        # compare the sequenuce number with that the node currently associates with its neighbor
+        # if the message's sequence number is greater, update the DV
+            # update the sequence number the node associates with the neighbor
+            # for every destination in the messages DV
+                # if the destination is in the node's DV, see if it needs to be updated
+                    # if min{cost(x, m)+cost(m,y)>cost(x,y)} update DV
+                # else, add a new entry in the DV for this destination
+        # else do nothing
         pass
 
     # Return a neighbor, -1 if no path to destination
     # Check routing table
     def get_next_hop(self, destination):
+        # see if the destination is a valid key in the DV
+        # if it is, get the first node in the path to the destinstion
+        # else return -1
         return -1
