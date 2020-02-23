6a60dd145f8d66cce955a1f6f6bc20daed1426b2
jdk <joshklein27@gmail.com>
2020-02-23 17:11:09 -0600

    Updates dv after message, still no optimal

--- distance_vector_node.py
+++ distance_vector_node.py
@@ -22,15 +22,14 @@ class Distance_Vector_Node(Node):
     # Need a function to update personal DV
 
     # Need a function to check for shorter times from other DVS after cost or personal DV is changed
-
+    # Use bellman ford here! maybe?
+    
     # Return a string
-
     def __str__(self):
         return "Rewrite this function to define your node dump printout"
 
-    # Called to inform you that an outgoing link to a neighbor has changed.
-    # It tells you that you can reach a certain neighbor (identified by an integer) with a certain latency.
-    # In response, you may want to update your tables and send further messages to your neighbors.
+    # Receives a new link cost then updates self.costs and self.dv given the new information
+    # Then calls to recalculate optimal DV using bellman-ford
     def link_has_been_updated(self, neighbor, latency):
         if latency == -1:  # delete a link
             if neighbor in self.neighbor_info:
@@ -39,7 +38,8 @@ class Distance_Vector_Node(Node):
             self.update_link_latency(neighbor, latency)
         else:  # create a link
             self.create_link(neighbor, latency)
-        self.send_to_neighbors(self.format_message())
+        # Check for short paths
+        # self.send_to_neighbors(self.format_message())
         pass
 
     def remove_link(self, neighbor_id):
@@ -54,7 +54,9 @@ class Distance_Vector_Node(Node):
         length_change = new_latency - self.neighbor_info[neighbor_id].latency
         self.neighbor_info[neighbor_id].latency = new_latency
         for destination in self.dv:
-            if self.dv[destination][1][0] == neighbor_id:
+            if (destination == neighbor_id) and (latency <= self.dv[destination][0]):
+                self.dv[destination] = [latency,[neighbor_id]]
+            elif self.dv[destination][1][0] == neighbor_id:
                 self.dv[destination][0] += length_change
         # check all other DVs to see if there exists a shorter path
 
@@ -63,18 +65,13 @@ class Distance_Vector_Node(Node):
         self.neighbor_info[neighbor_id] = new_neighbor
         # if there is no path to the new neighbor
         if neighbor_id not in self.dv:
-            self.dv[neighbor_id] = [
-                self.neighbor_info[neighbor_id].latency, [neighbor_id]]
+            self.dv[neighbor_id] = [latency, [neighbor_id]]
         # if there was already a path to the neighbor and the one-hop cost is cheaper than the older path
         elif self.neighbor_info[neighbor_id].latency < self.dv[neighbor_id][0]:
-            self.dv[neighbor_id] = [
-                self.neighbor_info[neighbor_id].latency, [neighbor_id]]
+            self.dv[neighbor_id] = [latency, [neighbor_id]]
 
-    # Called when a routing message "m" arrives at a node.
-    # This STRING message would have been sent by a neighbor
-    # In response, you may send further routing messages using self.send_to_neighbors or self.send_to_neighbor.
-    # You may also update your tables.
-
+    # Given a new neighbor DV, update self.dv
+    # Then call to optimize self.dv with bellman-ford
     def process_incoming_routing_message(self, m):
         json_object = json.loads(m)
         message_id = json_object["id"]
@@ -83,15 +80,20 @@ class Distance_Vector_Node(Node):
         # update the dv
         if message_seq_num > self.neighbor_info[message_id].seq_num:
             self.neighbor_info[message_id].dv = message_dv
-            self.update_dv(message_id)  # TBD
+            self.neighbor_info[message_id].seq_num = message_seq_num
+            
+            # Need to first update self.dv costs
+            self.update_personal_dv(message_id)
+            # Then check for optimal
+            
+            self.update_dv(message_id)  # TBD updates using bellman-ford
             # update the sequence number the node associates with the neighbor
             # for every destination in the messages DV
             # if the destination is in the node's DV, see if it needs to be updated
             # if min{cost(x, m)+cost(m,y)>cost(x,y)} update DV
             # else, add a new entry in the DV for this destination
             self.send_to_neighbors(self.format_message)
-        # else do nothing
-
+        # else do nothing the information is old
         pass
 
     # Return a neighbor, -1 if no path to destination
@@ -106,4 +108,22 @@ class Distance_Vector_Node(Node):
         json_message = {"id": self.id, "seq_num": self.seq_num, "dv": self.dv}
         return json.dumps(json_message)
 
-    self.update_dv():
+    def update_personal_dv(self, neighbor_id):
+        latency = self.neighbor_info[neighbor_id].latency
+        
+        for destination in self.neighbor_info[neighbor_id].dv:
+            new_latency = latency + self.neighbor_info[neighbor_id].dv[destintion][0]
+            new_path = neighbor_id + self.neighbor_info[neighbor_id].dv[destintion][1]
+            if destination in self.dv:
+                # Update according to bellman ford
+                if new_latency < self.dv[destination][0]:
+                    self.dv[destination][0] = new_latency
+                    self.dv[destination][1] = new_path
+            else:
+                self.dv[destination] = [new_latency, new_path]
+
+
+    # Distributed bellman ford to find any shorter paths
+    def bellman_ford(self):
+        for neighbor in self.dv
+
